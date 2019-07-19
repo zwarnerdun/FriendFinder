19140e348ac15240e9626c4e86fd8e7a3ca42a59
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-07-19 13:32:16 -0400

    updated stats for friends match based on score

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,8 +3,31 @@ var friends = [
         name: "Shanice",
 		photo: "http://www.elitetutoringnow.com/wp-content/uploads/avatars/3/5b5891eec3ade-bpfull.jpg",
 		scores: [1, 3, 5, 1, 1, 4, 2, 2, 5, 5]
-    }
+    },
 
+    {
+        name: "Rob",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKWwoi78mhzuSrpkExaZQ8wHXnDCuS_NExbUA5PN-paY9BZ9Amzw",
+		scores: [3, 3, 4, 4, 1, 2, 2, 2, 1, 1]
+    },
+
+    {
+        name: "Ashley",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSchnPTRjxJK1suhWVVZJtr-3Nm-10wCuj9pK2FzssSXvEvocLyAA",
+		scores: [2, 2, 2, 3, 3, 1, 4, 5, 5, 5]
+    },
+    
+    {
+        name: "Alanah",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbfK6HDJZNLQ4CUZ7hkkpwKYO_CgDdPqcAoCn7SbkP2nW-gYXE",
+		scores: [5, 5, 5, 2, 2, 1, 2, 3, 4, 5]
+    },
+
+    {
+        name: "Eli",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLzaNNQV3vLUjjC2TpVnOynFhyJ8cqN-QeyQTyMkMZgMLxUUm2",
+		scores: [3, 3, 4, 2, 1, 3, 2, 1, 1, 2]
+    }
 ];
 
 module.exports = friends;
