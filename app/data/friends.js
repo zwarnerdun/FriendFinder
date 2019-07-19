9799125bc98221b453310add0ffc63f72a52563d
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-07-18 20:15:51 -0400

    adding friends variable to store scores, name, and photo

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friends = [
+    {
+        name: "Shanice",
+		photo: "http://www.elitetutoringnow.com/wp-content/uploads/avatars/3/5b5891eec3ade-bpfull.jpg",
+		scores: [1, 3, 5, 1, 1, 4, 2, 2, 5, 5]
+    }
+
+];
+
+module.exports = friends;
